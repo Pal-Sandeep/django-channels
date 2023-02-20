b4c0270758b72ec9da5ab6c8390cf7090d0193d0
Sandeep Pal <skumar8work@gmail.com>
2023-02-20 23:10:19 +0530

    asgi configs added

--- DjangoChannels/asgi.py
+++ DjangoChannels/asgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
-
+from django.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from django.urls import path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoChannels.settings')
 
 application = get_asgi_application()
+
+ws_patterns = [
+    path('test/', )
+]
+
+application = ProtocolTypeRouter({
+    'websocket': URLRouter(ws_patterns)
+})
